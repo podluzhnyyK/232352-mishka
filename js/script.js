710ca84adcb6db30a8c983fea2949d49f8609b6d
podluzhyyK <podluzhnyy94@mail.ru>
2017-06-28 05:37:30 +0400

    Финальная проверка2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-var navMain = document.querySelector('.main-nav');
-var searchBasket = document.querySelector('.search-basket');
-var navToggle = document.querySelector('.page-header__toggle');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    searchBasket.classList.remove('search-basket--closed');
-    searchBasket.classList.add('search-basket--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    searchBasket.classList.add('search-basket--closed');
-    searchBasket.classList.remove('search-basket--opened')
-  }
-});
+//var navMain = document.querySelector(".main-nav");
+//var searchBasket = document.querySelector(".search-basket");
+//var navToggle = document.querySelector(".page-header__toggle");
+//
+//navToggle.addEventListener("click", function () {
+//  if (navMain.classList.contains("main-nav--closed")) {
+//    navMain.classList.remove("main-nav--closed");
+//    navMain.classList.add("main-nav--opened");
+//    searchBasket.classList.remove("search-basket--closed");
+//    searchBasket.classList.add("search-basket--opened");
+//    navToggle.classList.remove("page-header__toggle--opened");
+//    navToggle.classList.add("page-header__toggle--opened");
+//  } else {
+//    navMain.classList.add("main-nav--closed");
+//    navMain.classList.remove("main-nav--opened");
+//    searchBasket.classList.add("search-basket--closed");
+//    searchBasket.classList.remove("search-basket--opened")
+//    navToggle.classList.add("page-header__toggle--opened");
+//    navToggle.classList.remove("page-header__toggle--opened");
+//  }
+//});
